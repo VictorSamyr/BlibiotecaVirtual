fca4de60cdac24f6586c1a7a4afb9446a9026605
Nicolas Yan <nicolas07.yan@gmail.com>
2021-07-02 13:14:17 -0300

    Inserçao do usuario no banco de dados

--- Library/GeradorSHA256.java
+++ Library/GeradorSHA256.java
@@ -4,12 +4,17 @@ import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class GeradorSHA256 {
-    public static String getSHA256(String input) throws NoSuchAlgorithmException{
-        MessageDigest md = MessageDigest.getInstance("SHA256");
-        byte[] inputByte = input.getBytes(StandardCharsets.UTF_8);
-        byte[] shaByte = md.digest(inputByte); 
-        BigInteger number = new BigInteger(1, shaByte);
-        String hexString = number.toString(16);
-        return hexString;
+    public static String getSHA256(String input){
+        MessageDigest md;
+        try {
+            md = MessageDigest.getInstance("SHA256");
+            byte[] inputByte = input.getBytes(StandardCharsets.UTF_8);
+            byte[] shaByte = md.digest(inputByte); 
+            BigInteger number = new BigInteger(1, shaByte);
+            String hexString = number.toString(16);
+            return hexString;
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
